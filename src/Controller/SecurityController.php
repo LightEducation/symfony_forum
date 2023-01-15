2a2adacd72e80223bff95717e90c7f794eefd21d
LightEducation <idrissi27@gmail.com>
2023-01-15 22:23:13 +0000

    Setting up authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -14,9 +19,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -32,5 +37,26 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+
+    /**
+     * @Route ("/signup", name="signup")
+     */
+    public function signup(Request $request,UserRepository $userRepository,UserPasswordHasherInterface $hasher): Response
+    {
+        if($this->getUser())
+            return $this->redirectToRoute('home');
+        $user = new User();
+        $form = $this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $user->setPassword($hasher->hashPassword($user,$user->getPassword()));
+            $userRepository->add($user,true);
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render("security/signup.html.twig",[
+            "form" => $form->createView()
+        ]);
     }
 }
